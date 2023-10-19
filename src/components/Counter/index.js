93d7be6dcb6408f092692fcffa62085c78bbc19f
Roman Ratushnyi <freshcode1001@ukr.net>
2023-10-19 20:41:40 +0300

    Add language switch

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,30 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { increment, decrement, setStep } from '../../store/slices/counterSlice';
+import { setLang } from '../../store/slices/langSlice';
+import CONSTANTS from '../../constants';
+const {
+    LANGUAGE: {
+        EN_US, UA_UA, AR_AR, HE_HE
+    }
+} = CONSTANTS;
+
+const translations = new Map([
+    [
+        EN_US,
+        {
+            countText: 'Count',
+            stepText: 'Step',
+            incrementText: 'Increment',
+            decrementText: 'Decrement'
+        }
+    ],
+    [
+        UA_UA,
+        {
+            countText: 'Стан лічильника',
+            stepText: 'Крок',
+            incrementText: 'Збільшити',
+            decrementText: 'Зменшити'
+        }
+    ]
+])
 
 const Counter = (props) => {
-    const { count, step, increment, decrement, setStep } = props;
-    console.log(props);
+    const { count, step, language, increment, decrement, setStep, setLang } = props;
+    const translation = translations.get(language);
+    const { countText, stepText, incrementText, decrementText } = translation;
+    //console.log(props);
     return (
         <div>
-            <p>Count: {count}</p>
+            <select value={language} onChange={({ target: { value } }) => setLang(value) }>
+                <option value={EN_US}>English</option>
+                <option value={UA_UA}>Українська</option>
+            </select>
+            <p>{countText}: {count}</p>
             <label>
-                Step:
+                {stepText}:
                 <input 
                     type="number" 
                     value={step}
                     onChange={({ target: { value } }) => setStep(value)}
                 />
             </label>
-            <p>Step: {step}</p>
-            <button onClick={() => increment()}>Increment</button>
-            <button onClick={() => decrement()}>Decrement</button>
+            <p>{stepText}: {step}</p>
+            <button onClick={() => increment()}>{incrementText}</button>
+            <button onClick={() => decrement()}>{decrementText}</button>
         </div>
     );
 }
 
 function mapStateToProps(state) {
-    return state.counter;
+    return {
+        ...state.counter,
+        language: state.lang
+    };
 }
 
 // v1
@@ -40,7 +77,8 @@ function mapStateToProps(state) {
 const mapDispatchToProps = {
     increment,
     decrement,
-    setStep
+    setStep,
+    setLang
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
