9fa176b15a5356272e39f97fad0bd0edd9264dc3
Roman Ratushnyi <freshcode1001@ukr.net>
2023-10-20 20:44:43 +0300

    Add pages

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,13 @@
+import Header from 'components/Header';
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <h1>Home page</h1>
+        </>
+    );
+}
+
+export default HomePage;
